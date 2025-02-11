8b01345ed7fa1f333de969edd2449abfab0258c7
Emre Tatlici <92151030+EmreTatlici@users.noreply.github.com>
2025-02-11 12:52:48 +0300

    Update Contents.swift

--- Enumerations.playground/Contents.swift
+++ Enumerations.playground/Contents.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-
+// Defining an enum `Animals` with different animal cases
 enum Animals {
     case cat
     case dog
@@ -8,123 +8,112 @@ enum Animals {
     case hedgehog
 }
 
+// Creating an instance of `Animals` enum and assigning it to `cat`
 let cat = Animals.cat
 cat
 
+// Using a switch statement to handle each case of the `cat` instance
 switch cat {
 case .cat:
-    "This is a cat"
+    "This is a cat" // Prints when the case is `cat`
     break
 case .dog:
-    "This is a dog"
+    "This is a dog" // Prints when the case is `dog`
     break
-//case .rabbit:
-//    "This is a rabbit"
 case .rabbit:
-    "This is a rabbit"
+    "This is a rabbit" // Prints when the case is `rabbit`
     break
 case .hedgehog:
-    "This is a hedgehog"
+    "This is a hedgehog" // Prints when the case is `hedgehog`
     break
-//default:
-//    "This is something else" // switch must be exhaustive, meaning that it should have all the cases, when not, it has default
 }
 
+// Uncomment the following to see the warning about exhaustive switch statements
+// `switch` must be exhaustive (cover all cases) unless there's a `default` case.
+// default: "This is something else" // Uncomment to handle other cases if missing
 
+
+// Defining an enum `Shortcut` with associated values and making it Equatable
 enum Shortcut: Equatable {
     case fileOrFolder(path: URL, name: String)
     case wwwUrl(path: URL)
     case song(artist: String, songName: String)
 }
 
+// Creating an instance of `Shortcut` for a URL to Apple website
 let wwwApple = Shortcut.wwwUrl(
-    path: URL(string: "https:\\apple.com")! // use ! because the url might be nil, swift doesn't allow it
+    path: URL(string: "https:\\apple.com")! // Force unwrap because URL may be nil, Swift doesn't allow nil here
 )
 
-// In Swift, to compare an enum using the == operator, the enum must conform to the Equatable protocol.
+// Comparing two enum values (equatable conformance is required)
 if wwwApple == Shortcut.wwwUrl(path: URL(string: "https:\\apple.com")!) {
-    
+    // This block will execute if both instances are equal
 }
 
 
-
 switch wwwApple {
-    
-case let .fileOrFolder(
-    path,
-    name
-):
+case let .fileOrFolder(path, name):
+    // Accessing associated values of `fileOrFolder` case
     path
     name
     break
-    
-case let .wwwUrl(
-    path
-):
+case let .wwwUrl(path):
+    // Accessing associated value of `wwwUrl` case
     path
     break
-    
-case let .song(
-    artist, songName
-):
+case let .song(artist, songName):
+    // Accessing associated values of `song` case
     artist
     songName
     break
 }
 
+// `if case let` pattern matching allows us to extract associated values directly
 if case let .wwwUrl(path) = wwwApple {
     path
 }
 
+// Handling associated values for `song` case, and accessing `songName`
 let withoutYou = Shortcut.song(
     artist: "Symphony X",
     songName: "Without You"
 )
 
-if case let .song(_ , songName) = withoutYou {
+if case let .song(_, songName) = withoutYou {
     songName
 }
-if case let .song(artist , songName) = withoutYou {
+
+if case let .song(artist, songName) = withoutYou {
     artist
     songName
 }
 
 
+// Defining an enum `Vehicle` with cases that hold associated values
 enum Vehicle {
     case car(manufacturer: String, model: String)
     case bike(manufacturer: String, yearMade: Int)
     
+    // Computed property that returns the manufacturer of the vehicle
     var manufacturer: String {
         switch self {
         case let .car(manufacturer, _):
             return manufacturer
-            
         case let .bike(manufacturer, _):
             return manufacturer
         }
-
     }
 }
 
-
-//func getManufacturer(from vehicle: Vehicle) -> String { // or put that in enum
-//    switch vehicle {
-//    case let .car(manufacturer, _):
-//        return manufacturer
-//        
-//    case let .bike(manufacturer, yearMade):
-//        return manufacturer
-//    }
-//
-//}
-
+// Creating instances of `Vehicle`
 let car = Vehicle.car(manufacturer: "Tesla", model: "X")
-car.manufacturer
+car.manufacturer // Accessing the manufacturer of the car
 
 let bike = Vehicle.bike(manufacturer: "HD", yearMade: 1987)
-bike.manufacturer
+bike.manufacturer // Accessing the manufacturer of the bike
 
 
+// Defining an enum `FamilyMember` with raw values (String)
 enum FamilyMember: String {
     case father = "Dad"
     case mother = "Mom"
@@ -132,66 +121,69 @@ enum FamilyMember: String {
     case sister = "Sis"
 }
 
-FamilyMember.father.rawValue
+FamilyMember.father.rawValue  // Returns "Dad"
 
+
+// Defining an enum `FavoriteEmoji` that conforms to `CaseIterable` to access all cases
 enum FavoriteEmoji: String, CaseIterable {
     case rocky = "😜"
     case love = "🥰"
 }
 
-FavoriteEmoji.allCases
-FavoriteEmoji.allCases.map(\.rawValue) // .map converts all the values in arrays, dicts
+// Accessing all cases in the enum
+FavoriteEmoji.allCases  // Returns an array of all cases
+FavoriteEmoji.allCases.map(\.rawValue) // Maps the raw values of all cases into an array
 
+// Pattern matching with `rawValue` to find an emoji by its string
 if let love = FavoriteEmoji(rawValue: "🥰") {
-    "Found the love emoji"
+    "Found the love emoji"  // Will execute
     love
 } else {
-    "this emoji doesn't exist"
+    "This emoji doesn't exist"  // Will not execute
 }
 
-
-
+// Handling a case where an emoji doesn't exist in the enum
 if let snow = FavoriteEmoji(rawValue: "🌨️") {
     "Snow exists?! Really?"
     snow
 } else {
-    "As expected, snow doesn't exist in our enum"
+    "As expected, snow doesn't exist in our enum"  // Will execute
 }
 
+
+// Defining an enum `Height` with a mutating function to change its case
 enum Height {
     case short, medium, long
-    // when using mutating func, self (all cases in enum) can be mutated.
+    
+    // A mutating function to change the height to `.long`
     mutating func makeLong() {
         self = .long
     }
 }
 
 var myHeight = Height.medium
-myHeight.makeLong()
+myHeight.makeLong()  // Changes the value of `myHeight` to `.long`
 myHeight
 
 
+// Defining an indirect enum `IntOperation` to represent various mathematical operations
 indirect enum IntOperation {
     case add(Int, Int)
     case substract(Int, Int)
     case freeHand(IntOperation)
     
-    func calculateResult(
-        of operation: IntOperation? = nil
-    ) -> Int {
+    // Recursive function to calculate results of operations
+    func calculateResult(of operation: IntOperation? = nil) -> Int {
         switch operation ?? self {
         case let .add(lhs, rhs):
             return lhs + rhs
         case let .substract(lhs, rhs):
             return lhs - rhs
         case let .freeHand(operation):
-            return calculateResult(of: operation)
+            return calculateResult(of: operation) // Recursive call to calculate the result of a nested operation
         }
     }
 }
 
+// Example usage of `IntOperation`
 let freeHand = IntOperation.freeHand(.add(2, 4))
-
-
-
-
